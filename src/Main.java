749ef2028aeaab35030fc42aba27afafe5b870da
Dmytro Burdyga <dmytro.burdyga@gmail.com>
2016-06-07 21:48:24 +0300

    use lambdas in long form

--- src/Main.java
+++ src/Main.java
@@ -18,7 +18,18 @@ public class Main {
         }
     }
 
+    public static void usingLambdasInLongForm() {
+        List<Book> books = Books.all();
+        Collections.sort(books, (Book b1, Book b2) -> {
+            return b1.getTitle().compareTo(b2.getTitle());
+        });
+
+        for (Book book : books) {
+            System.out.println(book);
+        }
+    }
+
     public static void main(String[] args) {
-        usingAnonymousInlineClass();
+        usingLambdasInLongForm();
     }
 }
